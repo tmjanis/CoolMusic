8533b9ec392496163ceadf0ac7f950c9bb948b24
tmjanis <114050662+tmjanis@users.noreply.github.com>
2023-05-10 09:35:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const coolButton = document.querySelector('.js-cool');
+
+coolButton.addEventListener('click', () => {
+     window,open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley')
+
+})
